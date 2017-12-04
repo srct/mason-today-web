f8467fffa8bb3711378bfc50bc1442435aedba68
Landon DeCoito <rdecoito@gmu.edu>
2017-12-03 22:47:00 -0500

    code can now parse out a list of events from the xml

--- pscripts/parscript.py
+++ pscripts/parscript.py
@@ -4,11 +4,18 @@ import requests
 from bs4.diagnose import diagnose
 from bs4 import BeautifulSoup
 
-r = requests.get("http://25livepub.collegenet.com/calendars/events_all.xml")
-soup = BeautifulSoup(r.content, "lxml")
+#r = requests.get("http://25livepub.collegenet.com/calendars/events_all.xml")
+f = open("events.xml", "r")
+
+soup = BeautifulSoup(f.read(), "lxml")
 entries = soup.find_all('entry')
-tempent = entries
-print type(entries[0])
-for entry in entries:
-    ladle = BeautifulSoup(entry.toString(), "lxml")
-    entries.extend(ladle.find_all('title'))
+print type(entries[0]), "\n"
+print entries[0].prettify(), "\n"
+print entries[0].find('content').prettify(), "\n"
+
+titles = []
+for item in entries:
+    ladle = BeautifulSoup(unicode(item.string), "lxml")
+    titles = ladle.find_all('title')
+
+print titles
