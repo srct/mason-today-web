3bb91ef111e6790f9c373539e2b99b3574cfa837
Landon DeCoito <rdecoito@gmu.edu>
2017-12-06 02:18:08 -0500

    starting cleanup function and further defined Event class

--- pscripts/parscript.py
+++ pscripts/parscript.py
@@ -1,14 +1,20 @@
 print "and we begin"
 
-class event:
-    def __init__(self):
-        self.__name = "nameplaceholder"
-        self.__description = "contentplaceholder"
+class Event:
+    def __init__(self, entryTag): #where var entrySoup is a specific event tag
+        self.__name = entryTag.find('title').string
+        self.__description = entryTag.find('content').string
         self.__time = "timeplaceholder"
         self.__date = "dateplaceholder"
         self.__location = "locationplaceholder"
 
+    def __str__(self):
+        return self.__name + ": " + self.__description + "\n\n"
 
+def cleanup(string):
+    ctr = 0
+    while ctr < len(string):
+        
 
 from bs4 import BeautifulSoup
 import requests
@@ -32,6 +38,8 @@ for item in entries:
 print titles, "\n"
 
 
-
+events = []
 for item in entries:
-    print item.find('content').prettify(), "\n"
+    events.append(Event(item))
+
+print events
